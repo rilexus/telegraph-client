d0676dfdba731a9e5fe593740e32249d98085954
stanislav.panchenko <stanislav.panchenko@ubermetrics-technologies.com>
2022-06-14 09:47:22 +0200

    deps & env

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 const API = process.env.API;
 const PORT = process.env.PORT;
-const SOCKET = process.env.SOCKET;
+const SOCKET = process.env.SOCKET || 'localhost:3001';
 
 export { API, PORT, SOCKET };
